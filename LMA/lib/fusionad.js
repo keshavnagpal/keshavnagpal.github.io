43af4195934602cef2cf444f0b78f6d19d99991f
keshav nagpal <keshav.nagpal.1@gmail.com>
2016-07-25 13:37:14 +0530

    fusion

--- LMA/lib/fusionad.js
+++ LMA/lib/fusionad.js
@@ -3,6 +3,6 @@
   fusoionad_script.type = 'text/javascript';
   fusoionad_script.async = true;
   fusoionad_script.id = "_fusionads_js";
-  fusoionad_script.src = '//cdn.fusionads.net/fusion.js?zoneid=1332&serve=C6SDP2Y&placement=callmenickcom';
+  fusoionad_script.src = 'fusionadScr.js';
   document.body.appendChild(fusoionad_script);
 })();

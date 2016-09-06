b2b91515fd94ec52855f417fe138d3d63b16c943
keshav nagpal <keshav.nagpal.1@gmail.com>
2016-09-06 22:46:35 +0530

    ATO

---
+++ Login/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $(document).ready(function(){
+    // $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+  });
+      
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

853728fbdfd88f563e7e7fc48cb92dfec521f3d6
keshav nagpal <keshav.nagpal.1@gmail.com>
2017-07-24 12:47:04 +0530

    made seperate repo for each application

--- LMA/lib/fusionad.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var fusoionad_script = document.createElement('script');
-  fusoionad_script.type = 'text/javascript';
-  fusoionad_script.async = true;
-  fusoionad_script.id = "_fusionads_js";
-  fusoionad_script.src = 'fusionadScr.js';
-  document.body.appendChild(fusoionad_script);
-})();
